020a19e92035ae4f7f19d1e26e5f0e6edd78916f
Ezra Ashenafi <eazash22@gmail.com>
2022-03-21 11:53:25 +0300

    add assistant seed

--- util/seed.js
+++ util/seed.js
@@ -3,7 +3,7 @@ const bcrypt = require("bcrypt");
 
 async function main() {
   const adminPassword = process.env.ADMIN_PASSWORD;
-  const hashedPassword = await bcrypt.hash(adminPassword, 10);
+  const hashedAdminPassword = await bcrypt.hash(adminPassword, 10);
   const admin = await prisma.user.upsert({
     where: {
       username: "admin",
@@ -11,12 +11,27 @@ async function main() {
     update: {},
     create: {
       username: "admin",
-      password: hashedPassword,
+      password: hashedAdminPassword,
       role: "admin",
       fullName: "Administrator"
     },
   });
   console.log("Admin account created successfully");
+  const assistantPassword = process.env.ASSISTANT_PASSWORD;
+  const hashedAssistantPassword = await bcrypt.hash(assistantPassword, 10);
+  const assistantAdmin = await prisma.user.upsert({
+    where: {
+      username: "assistant-admin"
+    },
+    update: {},
+    create: {
+      username: "assistant-admin",
+      password: hashedAssistantPassword,
+      role: "assistant-admin",
+      fullName: "Assistant Admin"
+    }
+  })
+  console.log("Assistant Admin account Created");
 }
 
 main()
